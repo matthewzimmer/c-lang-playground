860201f20c3a49246784a3a5c0024534219f5a75
Matthew Zimmer <m16r01@gmail.com>
2019-02-06 17:01:29 -0500

    Sample code.

---
+++ playground/factorial.c
@@ -0,0 +1,17 @@
+//
+// Created by Matthew Zimmer on 2019-02-06.
+//
+
+#include <stdio.h>
+
+int fact(int n) {
+  if(n == 1) {
+    return 1;
+  } else {
+    return n * fact(n-1);
+  }
+}
+
+int main(void) {
+  printf("%i\n", fact(5));
+}
