026aea05f893780b732c6fb56e8187402bcb803a
Matthew Zimmer <m16r01@gmail.com>
2019-02-27 07:24:51 -0500

    402 HW playground

---
+++ csi402/increment_and_swap.c
@@ -0,0 +1,29 @@
+//
+// Created by Matthew Zimmer on 2019-02-27.
+//
+
+static void Swap(int* a, int* b) {
+  int temp;
+  temp = *a;
+  *a = *b;
+  *b = temp;
+}
+
+static void IncrementAndSwap(int *x, int* y) {
+  (*x)++;
+  (*y)++;
+  Swap(x, y); // don't need & here since a and b are already int*'s
+}
+
+int main() {
+  int alice = 10;
+  int bob = 20;
+
+  Swap(&alice, &bob);
+  // at this point, alice==20 and bob==10
+
+  IncrementAndSwap(&alice, &bob);
+  // at this point alice==11 and bob==21
+
+  return 0;
+}
